5c9a46328f177590ef371b147e23c5f86037a7b9
Jaya-x <techb23444@gmail.com>
2024-10-10 16:18:05 +0530

    Update App.jsx

--- src/components/Gallery/App.jsx
+++ src/components/Gallery/App.jsx
@@ -5,8 +5,7 @@ import Solidworks from './components/solidworks';
 import CentreOFun from './components/centr';
 import Farewell from './components/farewell';
 import Junkyard from './components/junk';
-import WaterRocket2K17 from './components/rocket_17';
-import WaterRocket2K22 from './components/rocket_22';
+import WaterRocket from './components/water-rocket';
 import './styles/index.css';
 function App() {
   return (
@@ -17,8 +16,7 @@ function App() {
         <Route path="/centre-o-fun" element={<CentreOFun />} />
         <Route path="/farewell" element={<Farewell />} />
         <Route path="/junkyard-wars" element={<Junkyard />} />
-        <Route path="/water-rocket-2k17" element={<WaterRocket2K17 />} />
-        <Route path="/water-rocket-2k22" element={<WaterRocket2K22 />} />
+        <Route path="/water-rocket" element={<WaterRocket />} />
       </Routes>
     
   );
